7b7041278c7c2bd571377268b9a0d5446819c158
Rishat Suleymanov <spk_irbis@mail.ru>
2021-12-03 20:26:29 +0300

    CityTestToString added

---
+++ src/test/java/ru/studre/entity/CityTest.java
@@ -0,0 +1,24 @@
+package ru.studre.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityTest {
+
+    @Test
+    void testToString() {
+        City city = new City(
+                1L,
+                "Адыгейск",
+                "Адыгея",
+                "Южный",
+                12248,
+                "1973");
+
+        Assertions.assertEquals(
+                "City{name='Адыгейск', region='Адыгея', district='Южный', population=12248, foundation='1973'}",
+                city.toString());
+    }
+}
