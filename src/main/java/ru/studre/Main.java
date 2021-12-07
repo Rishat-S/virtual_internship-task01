3a240e13a6a84c945a1fe339f60fb9091b8210e2
Rishat Suleymanov <spk_irbis@mail.ru>
2021-12-07 22:55:41 +0300

    Added solution for the third task

--- src/main/java/ru/studre/Main.java
+++ src/main/java/ru/studre/Main.java
@@ -1,13 +1,9 @@
 package ru.studre;
 
-import ru.studre.comparators.ByCityNameCaseInsensitive;
 import ru.studre.entity.City;
 import ru.studre.service.CityService;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 
 import static ru.studre.constants.Constants.CITY_DIRECTORY_FILE_PATH;
 
@@ -15,18 +11,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         CityService cityService = new CityService();
 
-        /* Второе задание */
-        // 2.1 Сортировка списка городов по наименованию в алфавитном порядке по убыванию без учета регистра
-        List<City> cityList = new ArrayList<>(cityService.getCities(CITY_DIRECTORY_FILE_PATH));
-        cityList.sort(new ByCityNameCaseInsensitive());
-        cityList.forEach(System.out::println);
+        /* Третье задание */
+        getCityWithMaxPopulation(cityService);
 
-        // 2.2 Сортировка списка городов по федеральному округу
-        // и наименованию города внутри каждого федерального округа в алфавитном порядке по убыванию с учетом регистра
-        cityService.getCities(CITY_DIRECTORY_FILE_PATH).stream()
-                .sorted(Comparator.comparing(City::getDistrict)
-                        .thenComparing(City::getName))
-                .forEach(System.out::println);
+    }
 
+    private static void getCityWithMaxPopulation(CityService cityService) throws IOException {
+        City[] cities = cityService.getCities(CITY_DIRECTORY_FILE_PATH).toArray(City[]::new);
+
+        Integer maxPopulation = cities[0].getPopulation();
+        int indexOfCityWithMaxPopulation = 0;
+        for (int i = 1; i < cities.length; i++) {
+            if (cities[i].getPopulation() > maxPopulation) {
+                maxPopulation = cities[i].getPopulation();
+                indexOfCityWithMaxPopulation = i;
+            }
+        }
+        System.out.println("[" + indexOfCityWithMaxPopulation + "]" + "=" + maxPopulation);
     }
 }
